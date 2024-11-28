3d229a07507d94926e0a81fae9c9c3796875a13b
shine26 <chews26@naver.com>
2024-11-28 10:59:22 +0900

    레벨 1-2: 환전 요청 중간 테이블 생성

---
+++ src/main/java/com/sparta/currency_user/entity/Exchange.java
@@ -0,0 +1,33 @@
+package com.sparta.currency_user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Exchange extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "currency_id", nullable = false)
+    private Currency currency;
+
+    private Long amount_in_krw;
+
+    private Long amount_after_exchange;
+
+    private String status;
+
+}
