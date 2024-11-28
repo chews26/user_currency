a881f9c59f490fad57819e0c81523cb25e1e3025
shine26 <chews26@naver.com>
2024-11-28 16:52:27 +0900

    레벨 2-5: 환전 entity 파일 dto 생성자 및 setter 추가

--- src/main/java/com/sparta/currency_user/entity/Exchange.java
+++ src/main/java/com/sparta/currency_user/entity/Exchange.java
@@ -1,12 +1,17 @@
 package com.sparta.currency_user.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+
+import java.math.BigDecimal;
 
 @Getter
 @Entity
@@ -24,10 +29,25 @@ public class Exchange extends BaseEntity {
     @JoinColumn(name = "currency_id", nullable = false)
     private Currency currency;
 
-    private Long amount_in_krw;
+    @Column(scale = 5)
+    private BigDecimal amount_in_krw;
 
-    private Long amount_after_exchange;
+    private BigDecimal amount_after_exchange;
 
     private String status;
 
+    protected Exchange() {}
+
+    // DTO 변환용 생성자
+    public Exchange(BigDecimal amount_in_krw, Currency currency, User user) {
+        this.amount_in_krw = amount_in_krw;
+        this.currency = currency;
+        this.user = user;
+        this.status = "normal"; // 기본 상태 설정
+    }
+
+    // 결과 값 저장용 Setter 메서드
+    public void setAmountAfterExchange(BigDecimal amountAfterExchange) {
+        this.amount_after_exchange = amountAfterExchange;
+    }
 }
