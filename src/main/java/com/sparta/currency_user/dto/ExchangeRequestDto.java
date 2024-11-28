7d6f0466329f233e69dd8721dc27ebf69830ad60
shine26 <chews26@naver.com>
2024-11-28 16:51:22 +0900

    레벨 2-3: 환전 Dto 파일 작성

--- src/main/java/com/sparta/currency_user/dto/ExchangeRequestDto.java
+++ src/main/java/com/sparta/currency_user/dto/ExchangeRequestDto.java
@@ -1,7 +1,35 @@
 package com.sparta.currency_user.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sparta.currency_user.entity.Currency;
+import com.sparta.currency_user.entity.Exchange;
+import com.sparta.currency_user.entity.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+
+import java.math.BigDecimal;
 
 @Getter
 public class ExchangeRequestDto {
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "0.00000")
+    private final BigDecimal amount_in_krw;
+
+    @NotBlank
+    private final String currency_name;
+
+    public ExchangeRequestDto(BigDecimal amount_in_krw, String currency_name) {
+        this.amount_in_krw = amount_in_krw;
+        this.currency_name = currency_name;
+    }
+
+    public Exchange toEntity(User user, Currency currency) {
+        return new Exchange(
+                this.amount_in_krw,
+                currency,
+                user
+        );
+    }
 }
--- src/main/java/com/sparta/currency_user/dto/ExchangeResponseDto.java
+++ src/main/java/com/sparta/currency_user/dto/ExchangeResponseDto.java
@@ -1,7 +1,41 @@
 package com.sparta.currency_user.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sparta.currency_user.entity.Exchange;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Getter
 public class ExchangeResponseDto {
+
+    private final String id;
+
+    private final String name;
+
+    private final String status;
+
+    private final BigDecimal amount_after_exchange;
+
+    private final String currency_name;
+
+    private final LocalDateTime created_at;
+
+    private final LocalDateTime modified_at;
+
+    public ExchangeResponseDto(String id, String name, String status, BigDecimal amount_after_exchange, String currency_name, LocalDateTime created_at, LocalDateTime modified_at) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.amount_after_exchange = amount_after_exchange;
+        this.currency_name = currency_name;
+        this.created_at = created_at;
+        this.modified_at = modified_at;
+    }
 }
+
+
